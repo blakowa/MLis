066862cd2d687d6e76912a552b0b8ab3a2808f33
BlaKow <morsik666@gmail.com>
2018-09-15 20:06:37 +0200

    Prostoknt

--- MLis_przyklady/If_8_4/Program.cs
+++ MLis_przyklady/If_8_4/Program.cs
@@ -10,7 +10,23 @@ namespace If_8_4
     {
         static void Main(string[] args)
         {
-            
+            int x = 1, y = 5, h = 4, w = 5;
+            int ptkx = 3, ptky = 4;
+
+            if ((ptkx > x  &  ptkx < w + x) & (ptky < y & ptky > y - h))
+            {
+                Console.WriteLine("Punkt o wspolrzencych x,y rownych = " +  ptkx + ',' +  ptky);
+                Console.WriteLine("Zawiera sie w prostokoncie o wspolrzenych lewgo gornego rogu x= " + x + " y = " + y + "wysokoœci = " + h + ", szerokosci = " + w);
+                Console.WriteLine("SUKCES!!!");
+            }
+            else
+            {
+                Console.WriteLine("Punkt o wspolrzencych x,y rownych = " + ptkx + ',' + ptky);
+                Console.WriteLine("NIE zawiera sie w prostokoncie o wspolrzenych lewgo gornego rogu x= " + x + " y = " + y + "wysokoœci = " + h + ", szerokosci = " + w);
+                Console.WriteLine("PORAZKA!!!");
+            }
+
+
         }
     }
 }
