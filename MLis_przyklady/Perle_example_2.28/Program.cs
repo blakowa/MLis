65664319e6a839eeab283c7bf509900f35847789
BlaKow <morsik666@gmail.com>
2018-09-16 22:54:02 +0200

    Petle

---
+++ MLis_przyklady/Perle_example_2.28/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perle_example_2._28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10;)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("[i = {0}] Petle w C#", i);
+                i++;
+            }
+
+            int j = 0;
+            for (; j < 10;)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[j = {0}] Petle w C#", j);
+                j++;
+            }
+
+            int k = 0;
+            for (; k++ < 10; )
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("[k = {0}] Petle w C#", k);
+            }
+        }
+    }
+}
---
+++ MLis_przyklady/Petle_przyklad_2.27/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petle_przyklad_2._27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("[i = {0}] Petle w C#", i);
+
+            }
+
+
+         }
+
+    }
+}
